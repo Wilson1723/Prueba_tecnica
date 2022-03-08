1707a2e0ac2b261e9003edfb3c814e974d9c294e
Wilson1723 <wilsonsistemas82@gmail.com>
2022-03-08 16:46:39 -0500

    Proyecto angular prueba tecnica

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,18 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDIhG12JgNRrnJI9s3Z4BN3zIyT7evLHRE",
+    authDomain: "empleados-bccc9.firebaseapp.com",
+    databaseURL: "https://empleados-bccc9.firebaseio.com",
+    projectId: "empleados-bccc9",
+    storageBucket: "empleados-bccc9.appspot.com",
+    messagingSenderId: "798540677359",
+    appId: "1:798540677359:web:2965abdd8a76f72db09b1a"
+  }
 };
 
 /*
@@ -13,4 +22,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
